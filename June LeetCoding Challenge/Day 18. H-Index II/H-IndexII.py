359c8dd367bddc8d7b522fe96b0388eeeb8d53da
Dhananjay Narayan <dhananjayashwath@gmail.com>
2020-06-18 22:49:45 +0530

    Create H-IndexII.py

---
+++ June LeetCoding Challenge/Day 18. H-Index II/H-IndexII.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n=len(citations)
+        l,r=0,n-1
+        while l<=r:
+            mid=(int)(l+(r-l)/2)
+            if citations[mid] < n-mid:
+                l=mid+1
+            else:
+                r=mid-1
+                
+        return n-l
